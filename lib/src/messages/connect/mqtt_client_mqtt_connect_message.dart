322a46494f2e65ef097ffc7cacb8e983e191c863
Steve Hamblett <steve.hamblett@linux.com>
2017-06-15 08:06:42 +0100

    Message class + tests

--- lib/src/messages/connect/mqtt_client_mqtt_connect_message.dart
+++ lib/src/messages/connect/mqtt_client_mqtt_connect_message.dart
@@ -9,4 +9,118 @@ part of mqtt_client;
 
 /// An Mqtt message that is used to initiate a connection to a message broker.
 class MqttConnectMessage extends MqttMessage {
+  /// Initializes a new instance of the MqttConnectMessage class.
+  /// Only called via the MqttMessage.create operation during processing of an Mqtt message stream.
+  MqttConnectMessage() {
+    this.header = new MqttHeader().asType(MqttMessageType.connect);
+    this.variableHeader = new MqttConnectVariableHeader();
+    this.payload = new MqttConnectPayload(this.variableHeader);
+  }
+
+  ///  Initializes a new instance of the <see cref="MqttConnectMessage" /> class.
+  MqttConnectMessage.fromByteBuffer(MqttHeader header,
+      MqttByteBuffer messageStream) {
+    this.header = header;
+    readFrom(messageStream);
+  }
+
+  /// Sets the name of the protocol to use.
+  MqttConnectMessage withProtocolName(String protocolName) {
+    this.variableHeader.protocolName = protocolName;
+    return this;
+  }
+
+  /// Sets the protocol version. (Defaults to v3, the only protcol version supported)
+  MqttConnectMessage withProtocolVersion(int protocolVersion) {
+    this.variableHeader.protocolVersion = protocolVersion;
+    return this;
+  }
+
+  /// Sets the startClean flag so that the broker drops any messages that were previously destined for us.
+  MqttConnectMessage startClean() {
+    this.variableHeader.connectFlags.cleanStart = true;
+    return this;
+  }
+
+  /// Sets the keep alive period
+  MqttConnectMessage keepAliveFor(int keepAliveSeconds) {
+    this.variableHeader.keepAlive = keepAliveSeconds;
+    return this;
+  }
+
+  /// Sets the Will flag of the variable header
+  MqttConnectMessage will() {
+    this.variableHeader.connectFlags.willFlag = true;
+    return this;
+  }
+
+  /// Sets the WillQos of the connect flag.
+  MqttConnectMessage withWillQos(MqttQos qos) {
+    this.variableHeader.connectFlags.willQos = qos;
+    return this;
+  }
+
+  /// Sets the WillRetain flag of the Connection Flags
+  MqttConnectMessage withWillRetain() {
+    this.variableHeader.connectFlags.willRetain = true;
+    return this;
+  }
+
+  /// Sets the client identifier of the message.
+  MqttConnectMessage withClientIdentifier(String clientIdentifier) {
+    this.payload.clientIdentifier = clientIdentifier;
+    return this;
+  }
+
+  /// Sets the will message.
+  MqttConnectMessage withWillMessage(String willMessage) {
+    this.payload.willMessage = willMessage;
+    return this;
+  }
+
+  /// Sets the Will Topic
+  MqttConnectMessage withWillTopic(String willTopic) {
+    this.payload.willTopic = willTopic;
+    return this;
+  }
+
+  /// Sets the authentication
+  MqttConnectMessage authenticateAs(String username, String password) {
+    this.variableHeader.connectFlags.usernameFlag = username.isNotEmpty;
+    this.variableHeader.connectFlags.passwordFlag = password.isNotEmpty;
+    this.payload.username = username;
+    this.payload.password = password;
+    return this;
+  }
+
+  /// The variable header contents. Contains extended metadata about the message
+  MqttConnectVariableHeader variableHeader;
+
+  /// The payload of the Mqtt Message.
+  MqttConnectPayload payload;
+
+  /// Writes the message to the supplied stream.
+  void writeTo(MqttByteBuffer messageStream) {
+    this.header.writeTo(
+        variableHeader.getWriteLength() + payload.getWriteLength(),
+        messageStream);
+    this.variableHeader.writeTo(messageStream);
+    this.payload.writeTo(messageStream);
+  }
+
+  /// Reads a message from the supplied stream.
+  void readFrom(MqttByteBuffer messageStream) {
+    this.variableHeader =
+    new MqttConnectVariableHeader.fromByteBuffer(messageStream);
+    this.payload = new MqttConnectPayload.fromByteBuffer(
+        this.variableHeader, messageStream);
+  }
+
+  String toString() {
+    final StringBuffer sb = new StringBuffer();
+    sb.writeln(super.toString());
+    sb.writeln(variableHeader.toString());
+    sb.writeln(payload.toString());
+    return sb.toString();
+  }
 }
