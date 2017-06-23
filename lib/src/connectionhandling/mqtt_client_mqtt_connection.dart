529cb8fcc0ba0c3727270ffc655c69fab6bfbab1
Steve Hamblett <steve.hamblett@linux.com>
2017-06-23 11:48:31 +0100

    Connection handling class updates

--- lib/src/connectionhandling/mqtt_client_mqtt_connection.dart
+++ lib/src/connectionhandling/mqtt_client_mqtt_connection.dart
@@ -7,26 +7,79 @@
 
 part of mqtt_client;
 
+/// Controls the read state used during async reads.
+enum ConnectionReadState {
+  /// Reading a message header.
+  header,
+
+  /// Reading message content.
+  content
+}
+
+
 /// The MQTT connection class
 class MqttConnection {
   /// The socket that maintains the connection to the MQTT broker.
   Socket tcpClient;
 
   /// Sync lock object to ensure that only a single message is sent through the connection handler at once.
-  Object _sendPadlock = new Object();
+  bool _sendPadlock = false;
 
   /// Initializes a new instance of the MqttConnection class.
   MqttConnection(String server, int port) {
     try {
-      // Connect and save the socket.
+      // Connect and save the socket. Do this lazily,
+      // we wont process anything until the connection state moves to
+      // connected.
       Socket.connect(server, port).then((socket) {
         tcpClient = socket;
+        _startListening();
+
       });
     } catch (SocketException) {
       final String message =
           "The connection to the message broker {$server}:{$port} could not be made.";
       throw new NoConnectionException(message);
     }
+  }
+
+  /// Create the listening stream subscription and subscribe the callbacks
+  void _startListening() {
+    tcpClient.listen(_onData,
+        onError: _onError,
+        onDone: _onDone);
+  }
+
+  /// OnData listener callback
+  void _onData(List<int> data) {
+
+  }
+
+  /// OnError listener callback
+  void _onError(Error error) {
+
+  }
+
+  /// OnDone listener callback
+  void _onDone() {
+
+  }
+
+  /// Disconnects from the message broker
+  void _disconnect() {
+    tcpClient.close();
+  }
+
+  /// Sends the message in the stream to the broker.
+  void send(MqttByteBuffer message) {
+    if (_sendPadlock) {
+      return;
+    } else {
+      _sendPadlock = true;
+    }
+    final typed.Uint8Buffer messageBytes = message.read(message.length);
+    tcpClient.add(messageBytes.toList());
+    _sendPadlock = false;
   }
 
   /// Initiate a new connection to a message broker
