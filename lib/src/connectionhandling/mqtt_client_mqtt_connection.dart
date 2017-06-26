b8daeff798e2e24584e359d618e8f6eb1a594bf6
Steve Hamblett <steve.hamblett@linux.com>
2017-06-26 08:17:49 +0100

    Connection handling class updates

--- lib/src/connectionhandling/mqtt_client_mqtt_connection.dart
+++ lib/src/connectionhandling/mqtt_client_mqtt_connection.dart
@@ -16,6 +16,40 @@ enum ConnectionReadState {
   content
 }
 
+/// State and logic used to read from the underlying network stream.
+class ReadWrapper {
+  /// Creates a new ReadWrapper that wraps the state used to read a message from a stream.
+  ReadWrapper() {
+    this.readState = ConnectionReadState.header;
+    this.messageBytes = new List<int>();
+    this.totalBytes = 1; // default to header read size.
+    this.nextReadSize = this.totalBytes;
+  }
+
+  /// The total bytes expected to be read from from the header of content
+  int totalBytes;
+
+  /// The bytes associated with the message being read.
+  List<int> messageBytes;
+
+  /// The amount of content to read during the next read.
+  int nextReadSize;
+
+  /// What is the connection currently reading.
+  ConnectionReadState readState;
+
+  /// A boolean that indicates whether the message read is complete
+  bool get isReadComplete => messageBytes.length < totalBytes;
+
+  /// Recalculates the number of bytes to read given the expected total size and the amount read so far.
+  void recalculateNextReadSize() {
+    if (totalBytes == 0) {
+      throw new SocketException(
+          "Total ReadBytes is 0, cannot calculate next read size.");
+    }
+    this.nextReadSize = totalBytes - messageBytes.length;
+  }
+}
 
 /// The MQTT connection class
 class MqttConnection {
@@ -25,6 +59,9 @@ class MqttConnection {
   /// Sync lock object to ensure that only a single message is sent through the connection handler at once.
   bool _sendPadlock = false;
 
+  /// The read wrapper
+  ReadWrapper readWrapper;
+
   /// Initializes a new instance of the MqttConnection class.
   MqttConnection(String server, int port) {
     try {
@@ -33,8 +70,8 @@ class MqttConnection {
       // connected.
       Socket.connect(server, port).then((socket) {
         tcpClient = socket;
+        readWrapper = new ReadWrapper();
         _startListening();
-
       });
     } catch (SocketException) {
       final String message =
@@ -45,25 +82,53 @@ class MqttConnection {
 
   /// Create the listening stream subscription and subscribe the callbacks
   void _startListening() {
-    tcpClient.listen(_onData,
-        onError: _onError,
-        onDone: _onDone);
+    tcpClient.listen(_onData, onError: _onError, onDone: _onDone);
   }
 
   /// OnData listener callback
   void _onData(List<int> data) {
-
+    // Protect against 0 bytes but should never happen.
+    if (data.length == 0) {
+      return;
+    }
+    readWrapper.messageBytes.addAll(data);
+    if (readWrapper.readState == ConnectionReadState.header) {
+      final typed.Uint8Buffer buff = new typed.Uint8Buffer();
+      buff.addAll(data);
+      final MqttByteBuffer stream = new MqttByteBuffer(buff);
+      final typed.Uint8Buffer lengthBytes = MqttHeader.readLengthBytes(stream);
+      final int remainingLength = MqttHeader.calculateLength(lengthBytes);
+      if (remainingLength == 0) {
+        //TODO FireDataAvailableEvent(readWrapper.MessageBytes);
+      } else {
+        // Total bytes of content is the remaining length plus the header.
+        readWrapper.totalBytes =
+            remainingLength + readWrapper.messageBytes.length;
+        readWrapper.recalculateNextReadSize();
+        readWrapper.readState = ConnectionReadState.content;
+      }
+    } else if (readWrapper.readState == ConnectionReadState.content) {
+      // If we haven't yet read all of the message repeat the read otherwise if
+      // we're finished process the message and switch back to waiting for the next header.
+      if (readWrapper.isReadComplete) {
+        // Reset the read buffer to accommodate the remaining length (last - what was read)
+        readWrapper.recalculateNextReadSize();
+      } else {
+        readWrapper.readState = ConnectionReadState.header;
+        //TODO FireDataAvailableEvent(readWrapper.MessageBytes);
+      }
+    }
+    // If we are reading a header then recreate the read wrapper for the next message
+    if (readWrapper.readState == ConnectionReadState.header) {
+      readWrapper = new ReadWrapper();
+    }
   }
 
   /// OnError listener callback
-  void _onError(Error error) {
-
-  }
+  void _onError(Error error) {}
 
   /// OnDone listener callback
-  void _onDone() {
-
-  }
+  void _onDone() {}
 
   /// Disconnects from the message broker
   void _disconnect() {
