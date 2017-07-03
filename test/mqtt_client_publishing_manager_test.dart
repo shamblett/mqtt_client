def02373616edde508b3e5f1bab6d50b8f374f9d
Steve Hamblett <steve.hamblett@linux.com>
2017-07-03 08:51:50 +0100

    Manager classes + tests

---
+++ test/mqtt_client_publishing_manager_test.dart
@@ -0,0 +1,18 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 27/06/2017
+ * Copyright :  S.Hamblett
+ */
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:test/test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:typed_data/typed_data.dart' as typed;
+
+// Mock classes
+
+void main() {
+  group("Message Identifier", () {
+    test("Numbering starts at 1", () {});
+  });
+}
