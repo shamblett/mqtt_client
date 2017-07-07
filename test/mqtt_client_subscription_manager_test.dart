76536593b6631c136e08a96226ef3ef5ffd4d63d
Steve Hamblett <steve.hamblett@linux.com>
2017-07-07 08:58:53 +0100

    Manager classes + tests

--- test/mqtt_client_subscription_manager_test.dart
+++ test/mqtt_client_subscription_manager_test.dart
@@ -1,9 +1,11 @@
+@Timeout(const Duration(seconds: 1))
 /*
  * Package : mqtt_client
  * Author : S. Hamblett <steve.hamblett@linux.com>
  * Date   : 27/06/2017
  * Copyright :  S.Hamblett
  */
+import 'dart:async';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:test/test.dart';
 import 'package:mockito/mockito.dart';
@@ -181,32 +183,59 @@ void main() {
           subs.getSubscriptionsStatus(topic), SubscriptionStatus.doesNotExist);
     });
     test("Change notification", () {
+      int recCount = 0;
+      const String topic = "testtopic";
+      StreamSubscription st;
+      ChangeNotifier<MqttReceivedMessage> cn;
+      void subRec(List<MqttReceivedMessage> c) {
+        expect(c[0].topic, topic);
+        expect(c[0].payload, new isInstanceOf<MqttPublishMessage>());
+        final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+        if (recCount == 0) {
+          expect(recMess.variableHeader.messageIdentifier, 1);
+          expect(MqttPublishPayload.bytesToString(recMess.payload.message),
+              "<100><101><97><100>");
+          expect(recMess.header.qos, MqttQos.atLeastOnce);
+          recCount++;
+        } else {
+          expect(recMess.variableHeader.messageIdentifier, 2);
+          expect(MqttPublishPayload.bytesToString(recMess.payload.message),
+              "<109><101><97><116>");
+          expect(recMess.header.qos, MqttQos.atMostOnce);
+        }
+      }
+      final t1 = expectAsync1(subRec, count: 1);
       testCHS.sentMessages.clear();
       final PublishingManager pm = new PublishingManager(testCHS);
-      const String topic = "testtopic";
       const MqttQos qos = MqttQos.atLeastOnce;
       final SubscriptionsManager subs = new SubscriptionsManager(testCHS, pm);
-      final ChangeNotifier<MqttReceivedMessage> cn =
-      subs.registerSubscription(topic, qos);
+      cn = subs.registerSubscription(topic, qos);
+      // Start listening
+      st = cn.changes.listen(subRec);
       // Publish messages on the topic
       final typed.Uint8Buffer buff = new typed.Uint8Buffer(4);
       buff[0] = 'd'.codeUnitAt(0);
       buff[1] = 'e'.codeUnitAt(0);
       buff[2] = 'a'.codeUnitAt(0);
       buff[3] = 'd'.codeUnitAt(0);
-      pm.publish(new PublicationTopic("topic"), MqttQos.atMostOnce, buff);
+      final MqttPublishMessage pubMess = new MqttPublishMessage()
+          .publishData(buff)
+          .toTopic(topic)
+          .withMessageIdentifier(1)
+          .withQos(MqttQos.atLeastOnce);
+      pm.handlePublish(pubMess);
       buff[0] = 'm'.codeUnitAt(0);
       buff[1] = 'e'.codeUnitAt(0);
       buff[2] = 'a'.codeUnitAt(0);
       buff[3] = 't'.codeUnitAt(0);
-      pm.publish(new PublicationTopic("topic"), MqttQos.atMostOnce, buff);
-      // Listen for the subscriptions
-      final t1 = expectAsync0(() {
-        cn.changes.listen((c) {
-          print("Change notification:: listening ${c.toString()}");
-        });
-      });
-      t1;
+      final MqttPublishMessage pubMess1 = new MqttPublishMessage()
+          .publishData(buff)
+          .toTopic(topic)
+          .withMessageIdentifier(2)
+          .withQos(MqttQos.atMostOnce);
+      pm.handlePublish(pubMess1);
+      //Stop listening
+      st.cancel();
     });
   });
 }
