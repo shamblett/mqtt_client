74e6448cbfaa7d56fdcf47e4def570036dbd25cf
shamblett <stephen.hamblett@linux.com>
2017-07-10 15:35:04 +0100

    The client tests

--- test/mqtt_client_test.dart
+++ test/mqtt_client_test.dart
@@ -5,16 +5,9 @@
  * Copyright :  S.Hamblett
  */
 import 'package:mqtt_client/mqtt_client.dart';
-import 'dart:async';
-import 'package:test/test.dart';
 
-void main() {
-  group("Client", () {
-    test("Broker Connect", () async {
-      final MqttClient client =
+main() async {
+  final MqttClient client =
       new MqttClient("test.mosquitto.org", "SJHMQTTClient");
-      final ConnectionState state = await client.connect();
-      expect(state, ConnectionState.connected);
-    });
-  });
+  final ConnectionState state = await client.connect();
 }
