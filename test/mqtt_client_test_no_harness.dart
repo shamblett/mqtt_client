649d3fe0b9bce4f0dda57c52718dc2570cf48191
Steve Hamblett <steve.hamblett@linux.com>
2017-07-11 13:30:47 +0100

    The client tests

--- test/mqtt_client_test_no_harness.dart
+++ test/mqtt_client_test_no_harness.dart
@@ -7,13 +7,32 @@
 import 'dart:io';
 import 'dart:async';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:observable/observable.dart';
 
 main() async {
   MqttLogger.loggingOn = true;
+  // Connect the client
   final MqttClient client =
   new MqttClient("test.mosquitto.org", "SJHMQTTClient");
   final ConnectionState state = await client.connect();
-  print("Mosquitto client connected");
+  if (state == ConnectionState.connected) {
+    print("Mosquitto client connected");
+  } else {
+    print("ERROR Mosquitto client connection failed - disconnecting");
+    client.disconnect();
+  }
+  // Subscribe to a known topic
+  final String topic = "test/hw";
+  final ChangeNotifier<MqttReceivedMessage> cn =
+  client.listenTo(topic, MqttQos.atLeastOnce);
+  cn.changes.listen((List<MqttReceivedMessage> c) {
+    final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+    final String pt =
+    MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+    print("Change notification:: payload is $pt for topic $topic");
+  });
+  print("Sleeping....");
+  await MqttUtilities.asyncSleep(60);
+  print("Disconnecting");
   client.disconnect();
-  print("Mosquitto client disconnected");
 }
