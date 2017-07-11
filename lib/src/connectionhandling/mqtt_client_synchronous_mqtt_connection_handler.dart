7f2c3748107558ea799578bac186b4daed939eb9
Steve Hamblett <steve.hamblett@linux.com>
2017-07-11 10:28:32 +0100

    The client tests

--- lib/src/connectionhandling/mqtt_client_synchronous_mqtt_connection_handler.dart
+++ lib/src/connectionhandling/mqtt_client_synchronous_mqtt_connection_handler.dart
@@ -32,7 +32,7 @@ class SynchronousMqttConnectionHandler extends MqttConnectionHandler
       MqttLogger.log(
           "SynchronousMqttConnectionHandler::internalConnect - pre sleep, state = $connectionState");
       // We're the sync connection handler so we need to wait for the brokers acknowledgement of the connections
-      await MqttUtilities.asyncSleep(5);
+      //await MqttUtilities.asyncSleep(5);
       MqttLogger.log(
           "SynchronousMqttConnectionHandler::internalConnect - post sleep, state = $connectionState");
     } while (connectionState != ConnectionState.connected &&
