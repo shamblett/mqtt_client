6d039c972612bc9e6e34aa792a9953a264066b02
Steve Hamblett <steve.hamblett@linux.com>
2017-07-12 11:28:38 +0100

    Prep for release

---
+++ example/usage.dart
@@ -0,0 +1,9 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 31/05/2017
+ * Copyright :  S.Hamblett
+ */
+
+/// An annotated simple subscribe/publish usage example for mqtt_client
+
