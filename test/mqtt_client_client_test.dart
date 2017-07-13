5efd5a2ff8e9719b39ebcda0a796659389bf1b6a
Steve Hamblett <steve.hamblett@linux.com>
2017-07-13 07:44:02 +0100

    Prep for release [ci skip]

---
+++ test/mqtt_client_client_test.dart
@@ -0,0 +1,39 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 11/07/2017
+ * Copyright :  S.Hamblett
+ */
+@TestOn("linux")
+import 'dart:io';
+import 'dart:async';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:test/test.dart';
+
+/// These tests check the mqtt client subscribe/publish functionality against a publicly
+/// available(Mosquitto) MQTT broker.
+/// The tests are restricted to a linux environment for no other reason than my windows development
+/// box is firewalled and the tests will fail, if you wish to run these tests on Windows please remove
+/// the TestOn annotation. The servers are pinged first just to ensure they are up before the tests are run.
+
+/// Helper function to ping a server
+bool pingServer(String server) {
+  final ProcessResult result = Process.runSync('ping', ['-c3', '$server']);
+  // Get the exit code from the new process.
+  if (result.exitCode == 0) {
+    return false;
+  } else {
+    print("Server - $server is dead, exit code is ${result
+        .exitCode} - skipping");
+    return true;
+  }
+}
+
+void main() {
+  final bool skipTests = pingServer("test.mosquitto.org");
+  test("Subscribe", () {
+    final ProcessResult result =
+    Process.runSync('dart', ['mqtt_client_broker_test_subscribe.dart']);
+    expect(result.exitCode, 0);
+  }, skip: skipTests);
+}
