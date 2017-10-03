582f48e4f13ceacd291686720d53a8d295cb7880
Steve Hamblett <steve.hamblett@linux.com>
2017-10-03 13:48:31 +0100

    Iotcore test updates [ci skip]

--- test/mqtt_client_broker_test_iotcore.dart
+++ test/mqtt_client_broker_test_iotcore.dart
@@ -6,7 +6,6 @@
  */
 import 'dart:io';
 import 'dart:async';
-import 'package:typed_data/typed_data.dart' as typed;
 import 'package:mqtt_client/mqtt_client.dart';
 
 Future<int> main() async {
@@ -14,8 +13,8 @@ Future<int> main() async {
   final String url = "mqtt.googleapis.com";
   final int port = 8883;
   final String clientId = "projects/warm-actor-356/locations/europe-west1/registries/home-sensors/devices/dummy-sensor";
-  final String username = "";
-  final password = "";
+  final String username = "unused";
+  final password = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIiLCJpYXQiOjE1MDcwMzQzMzQsImV4cCI6MTUzODU3MDU2OCwiYXVkIjoid2FybS1hY3Rvci0zNTYiLCJzdWIiOiIifQ.FIhwvyj0LS0zyoQ_uendUpMWQEeb5eNg7t4LN9NnVvY";
   final MqttClient client =
   new MqttClient(url, clientId);
   client.port = port;
@@ -35,17 +34,17 @@ Future<int> main() async {
     client.disconnect();
   }
   // Publish a known topic
-  final String topic = "Dart/SJH/mqtt_client";
-  final typed.Uint8Buffer buff = new typed.Uint8Buffer(5);
-  buff[0] = 'h'.codeUnitAt(0);
-  buff[1] = 'e'.codeUnitAt(0);
-  buff[2] = 'l'.codeUnitAt(0);
-  buff[3] = 'l'.codeUnitAt(0);
-  buff[4] = 'o'.codeUnitAt(0);
-  client.publishMessage(topic, MqttQos.exactlyOnce, buff);
-  print("Sleeping....");
-  await MqttUtilities.asyncSleep(10);
-  print("Disconnecting");
-  client.disconnect();
+//  final String topic = "Dart/SJH/mqtt_client";
+//  final typed.Uint8Buffer buff = new typed.Uint8Buffer(5);
+//  buff[0] = 'h'.codeUnitAt(0);
+//  buff[1] = 'e'.codeUnitAt(0);
+//  buff[2] = 'l'.codeUnitAt(0);
+//  buff[3] = 'l'.codeUnitAt(0);
+//  buff[4] = 'o'.codeUnitAt(0);
+//  client.publishMessage(topic, MqttQos.exactlyOnce, buff);
+//  print("Sleeping....");
+//  await MqttUtilities.asyncSleep(10);
+//  print("Disconnecting");
+//  client.disconnect();
   return 0;
 }
