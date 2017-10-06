84d2297d791d99c163c49417985ee93966f875d0
Steve Hamblett <steve.hamblett@linux.com>
2017-10-06 11:31:16 +0100

    Prep for release - test tidy up [ci skip]

--- test/mqtt_client_connection_tcp_test.dart
+++ test/mqtt_client_connection_tcp_test.dart
@@ -84,15 +84,6 @@ void main() {
       });
       t1();
     });
-    test("Connect no connect ack", () {
-      final SynchronousMqttConnectionHandler ch =
-      new SynchronousMqttConnectionHandler();
-      final t1 = expectAsync0(() {
-        ch.connect(mockBrokerAddress, mockBrokerPort,
-            new MqttConnectMessage().withClientIdentifier(testClientId));
-      });
-      t1();
-    });
     test("Successful response and disconnect", () async {
       final MockBroker broker = new MockBroker();
 
