0dce0db999a87f521f9cf77cd037ed4205e19cd4
Steve Hamblett <steve.hamblett@linux.com>
2017-10-09 11:49:20 +0100

    Prep for release - test updates [ci skip]

--- test/mqtt_client_connection_ws_test.dart
+++ test/mqtt_client_connection_ws_test.dart
@@ -6,9 +6,13 @@
  */
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:test/test.dart';
+import 'package:typed_data/typed_data.dart' as typed;
+import 'mqtt_client_mockbroker.dart';
 
 void main() {
   // Test wide variables
+  final MockBrokerWs brokerWs = new MockBrokerWs();
+  final String mockBrokerAddressWs = "ws://localhost/ws";
   final String mockBrokerAddressWsNoScheme = "localhost.com";
   final String mockBrokerAddressWsBad = "://localhost.com";
   final int mockBrokerPortWs = 8080;
@@ -45,6 +49,53 @@ void main() {
             "mqtt-client::NoConnectionException: "
                 "MqttWsConnection::The URI supplied for the WS has an incorrect scheme - $mockBrokerAddressWsNoScheme");
       }
+    });
+  }, skip: false);
+
+  group("Connection Keep Alive - Mock broker WS", () {
+    test("Successful response WS", () async {
+      int expectRequest = 0;
+
+      void messageHandlerConnect(typed.Uint8Buffer messageArrived) {
+        final MqttConnectAckMessage ack = new MqttConnectAckMessage()
+            .withReturnCode(MqttConnectReturnCode.connectionAccepted);
+        brokerWs.sendMessage(ack);
+      }
+
+      void messageHandlerPingRequest(typed.Uint8Buffer messageArrived) {
+        final MqttByteBuffer headerStream = new MqttByteBuffer(messageArrived);
+        final MqttHeader header = new MqttHeader.fromByteBuffer(headerStream);
+        if (expectRequest <= 3) {
+          print(
+              "WS Connection Keep Alive - Successful response - Ping Request received $expectRequest");
+          expect(header.messageType, MqttMessageType.pingRequest);
+          expectRequest++;
+        }
+      }
+
+      await brokerWs.start();
+      final SynchronousMqttConnectionHandler ch =
+      new SynchronousMqttConnectionHandler();
+      ch.useWebSocket = true;
+      brokerWs.setMessageHandler(messageHandlerConnect);
+      await ch.connect(mockBrokerAddressWs, mockBrokerPortWs,
+          new MqttConnectMessage().withClientIdentifier(testClientId));
+      expect(ch.connectionState, ConnectionState.connected);
+      brokerWs.setMessageHandler(messageHandlerPingRequest);
+      final MqttConnectionKeepAlive ka = new MqttConnectionKeepAlive(ch, 2);
+      print(
+          "WS Connection Keep Alive - Successful response - keepealive ms is ${ka
+              .keepAlivePeriod}");
+      print(
+          "WS Connection Keep Alive - Successful response - ping timer active is ${ka
+              .pingTimer.isActive.toString()}");
+      final Stopwatch stopwatch = new Stopwatch()
+        ..start();
+      await MqttUtilities.asyncSleep(10);
+      print("WS Connection Keep Alive - Successful response - Elapsed time "
+          "is ${stopwatch.elapsedMilliseconds / 1000} seconds");
+      ka.stop();
+      brokerWs.close();
     });
   }, skip: false);
 }
