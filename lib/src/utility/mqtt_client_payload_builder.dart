e68e75922a3f6933ec5a6f56bb1a030571e1c6e5
Steve Hamblett <steve.hamblett@linux.com>
2018-04-19 07:28:42 +0100

    Add the payload builder package

--- lib/src/utility/mqtt_client_payload_builder.dart
+++ lib/src/utility/mqtt_client_payload_builder.dart
@@ -19,8 +19,23 @@ class MqttClientPayloadBuilder {
     _payload = new typed.Uint8Buffer();
   }
 
-  ///
-  void addByte(int val) {
-    _payload.add(new typed.Uint8Buffer(1)..add(val));
+  /// Add a buffer
+  void addBuffer(typed.Uint8Buffer buffer) {
+    _payload.addAll(buffer);
   }
+
+  /// Add byte
+  void addByte(int val) {
+    final typed.Uint8Buffer tmp = new typed.Uint8Buffer(1);
+    tmp.add(val);
+    _payload.add(tmp.toList()[0]);
+  }
+
+  /// Add a bool, true is 1, false is 0
+  void addBool(bool val) {
+    val ? addByte(1) : addByte(0);
+  }
+
+/// Add a halfword, 16 bits
+
 }
