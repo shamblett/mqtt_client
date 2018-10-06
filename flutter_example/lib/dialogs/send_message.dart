cbeb34f5b6198e0fdd3af6bd0708ecbd3b9a36e9
Ben Hagen <c.ben.hagen@gmail.com>
2018-10-06 11:41:44 +0200

    Add flutter example

---
+++ flutter_example/lib/dialogs/send_message.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+
+class SendMessageDialog extends StatefulWidget {
+  final mqtt.MqttClient client;
+
+  const SendMessageDialog({Key key, @required this.client}) : super(key: key);
+
+  @override
+  _SendMessageDialogState createState() => _SendMessageDialogState();
+}
+
+class _SendMessageDialogState extends State<SendMessageDialog> {
+  bool _hasMessage = false;
+  bool _hasTopic = false;
+  bool _retainValue = false;
+  bool _saveNeeded = false;
+  int _qosValue = 0;
+  String _messageContent;
+  String _topicContent;
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(title: Text('New message'), actions: <Widget>[
+        FlatButton(
+            child: Text('SEND',
+                style: theme.textTheme.body1.copyWith(color: Colors.white)),
+            onPressed: () {
+              if (_formKey.currentState.validate()) {
+                _formKey.currentState.save();
+                _sendMessage();
+              }
+            })
+      ]),
+      body: Form(
+        key: _formKey,
+        onWillPop: _onWillPop,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              alignment: Alignment.bottomLeft,
+              child: TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Message', filled: true),
+                style: theme.textTheme.headline,
+                maxLines: 2,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                },
+                onSaved: (String value) {
+                  setState(() {
+                    _hasMessage = value.isNotEmpty;
+                    if (_hasMessage) {
+                      _messageContent = value;
+                    }
+                  });
+                },
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              alignment: Alignment.bottomLeft,
+              child: TextFormField(
+                decoration:
+                    const InputDecoration(labelText: 'Topic', filled: true),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                },
+                onSaved: (String value) {
+                  setState(() {
+                    _hasTopic = value.isNotEmpty;
+                  });
+                  if (_hasTopic) {
+                    _topicContent = value;
+                  }
+                },
+              ),
+            ),
+            _buildQosChoiceChips(),
+            Container(
+              decoration: BoxDecoration(
+                  border:
+                      Border(bottom: BorderSide(color: theme.dividerColor))),
+              child: Row(
+                children: <Widget>[
+                  Checkbox(
+                      value: _retainValue,
+                      onChanged: (bool value) {
+                        setState(() {
+                          _retainValue = value;
+                          _saveNeeded = true;
+                        });
+                      }),
+                  const Text('Retained'),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Wrap _buildQosChoiceChips() {
+    return Wrap(
+      spacing: 4.0,
+      children: List<Widget>.generate(
+        3,
+        (int index) {
+          return ChoiceChip(
+            label: Text('QoS level $index'),
+            selected: _qosValue == index,
+            onSelected: (bool selected) {
+              setState(() {
+                _qosValue = selected ? index : null;
+              });
+            },
+          );
+        },
+      ).toList(),
+    );
+  }
+
+  Future<bool> _onWillPop() async {
+    _saveNeeded = _hasTopic || _hasMessage || _saveNeeded;
+
+    if (!_saveNeeded) return true;
+
+    final ThemeData theme = Theme.of(context);
+    final TextStyle dialogTextStyle =
+        theme.textTheme.subhead.copyWith(color: theme.textTheme.caption.color);
+
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              content: Text('Discard message?', style: dialogTextStyle),
+              actions: <Widget>[
+                FlatButton(
+                    child: const Text('CANCEL'),
+                    onPressed: () {
+                      Navigator.of(context).pop(
+                          false); // Pops the confirmation dialog but not the page.
+                    }),
+                FlatButton(
+                    child: const Text('DISCARD'),
+                    onPressed: () {
+                      Navigator.of(context).pop(
+                          true); // Returning true to _onWillPop will pop again.
+                    })
+              ],
+            );
+          },
+        ) ??
+        false;
+  }
+
+  void _sendMessage() {
+    final mqtt.MqttClientPayloadBuilder builder =
+        mqtt.MqttClientPayloadBuilder();
+
+    builder.addString(_messageContent);
+    widget.client.publishMessage(
+      _topicContent,
+      mqtt.MqttQos.values[_qosValue],
+      builder.payload,
+      _retainValue,
+    );
+    Navigator.pop(context);
+  }
+}
---
+++ flutter_example/lib/main.dart
@@ -0,0 +1,419 @@
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:mqtt_client_example/models/message.dart';
+import 'package:mqtt_client_example/dialogs/send_message.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  PageController _pageController;
+  int _page = 0;
+
+  String broker = 'test.mosquitto.org';
+  mqtt.MqttClient client;
+  mqtt.ConnectionState connectionState;
+
+  StreamSubscription subscription;
+
+  TextEditingController topicController = TextEditingController();
+  Set<String> topics = Set<String>();
+
+  List<Message> messages = <Message>[];
+  ScrollController messageController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    IconData connectionStateIcon;
+    switch (client?.connectionState) {
+      case mqtt.ConnectionState.connected:
+        connectionStateIcon = Icons.cloud_done;
+        break;
+      case mqtt.ConnectionState.disconnected:
+        connectionStateIcon = Icons.cloud_off;
+        break;
+      case mqtt.ConnectionState.connecting:
+        connectionStateIcon = Icons.cloud_upload;
+        break;
+      case mqtt.ConnectionState.disconnecting:
+        connectionStateIcon = Icons.cloud_download;
+        break;
+      case mqtt.ConnectionState.faulted:
+        connectionStateIcon = Icons.error;
+        break;
+      default:
+        connectionStateIcon = Icons.cloud_off;
+    }
+    void navigationTapped(int page) {
+      _pageController.animateToPage(page,
+          duration: const Duration(milliseconds: 300), curve: Curves.ease);
+    }
+
+    void onPageChanged(int page) {
+      setState(() {
+        this._page = page;
+      });
+    }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(broker),
+              SizedBox(
+                width: 8.0,
+              ),
+              Icon(connectionStateIcon),
+            ],
+          ),
+        ),
+        floatingActionButton: _page == 2
+            ? Builder(builder: (BuildContext context) {
+                return FloatingActionButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute<String>(
+                          builder: (BuildContext context) =>
+                              SendMessageDialog(client: client),
+                          fullscreenDialog: true,
+                        ));
+                  },
+                );
+              })
+            : null,
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: navigationTapped,
+          currentIndex: _page,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.cloud),
+              title: Text('Broker'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.playlist_add),
+              title: Text('Subscriptions'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+          ],
+        ),
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: onPageChanged,
+          children: <Widget>[
+            _buildBrokerPage(connectionStateIcon),
+            _buildSubscriptionsPage(),
+            _buildMessagesPage(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column _buildBrokerPage(IconData connectionStateIcon) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              broker,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            SizedBox(width: 8.0),
+            Icon(connectionStateIcon),
+          ],
+        ),
+        SizedBox(height: 8.0),
+        RaisedButton(
+          child: Text(client?.connectionState == mqtt.ConnectionState.connected
+              ? 'Disconnect'
+              : 'Connect'),
+          onPressed: () {
+            if (client?.connectionState == mqtt.ConnectionState.connected) {
+              _disconnect();
+            } else {
+              _connect();
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  Column _buildMessagesPage() {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView(
+            controller: messageController,
+            children: _buildMessageList(),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(
+            child: Text('Clear'),
+            onPressed: () {
+              setState(() {
+                messages.clear();
+              });
+            },
+          ),
+        )
+      ],
+    );
+  }
+
+  Column _buildSubscriptionsPage() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 200.0,
+              child: TextField(
+                controller: topicController,
+                onSubmitted: (String topic) {
+                  _subscribeToTopic(topic);
+                },
+                decoration: InputDecoration(hintText: 'Please enter a topic'),
+              ),
+            ),
+            SizedBox(width: 8.0),
+            RaisedButton(
+              child: Text('add topic'),
+              onPressed: () {
+                _subscribeToTopic(topicController.value.text);
+              },
+            ),
+          ],
+        ),
+        SizedBox(height: 16.0),
+        Wrap(
+          spacing: 8.0,
+          runSpacing: 4.0,
+          alignment: WrapAlignment.start,
+          children: _buildTopicList(),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    subscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    super.initState();
+  }
+
+  List<Widget> _buildMessageList() {
+    return messages
+        .map((Message message) => Card(
+              color: Colors.white70,
+              child: ListTile(
+                trailing: CircleAvatar(
+                    radius: 14.0,
+                    backgroundColor: Theme.of(context).accentColor,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'QoS',
+                          style: TextStyle(fontSize: 8.0),
+                        ),
+                        Text(
+                          message.qos.index.toString(),
+                          style: TextStyle(fontSize: 8.0),
+                        ),
+                      ],
+                    )),
+                title: Text(message.topic),
+                subtitle: Text(message.message),
+                dense: true,
+              ),
+            ))
+        .toList()
+        .reversed
+        .toList();
+  }
+
+  List<Widget> _buildTopicList() {
+    // Sort topics
+    final List<String> sortedTopics = topics.toList()
+      ..sort((String a, String b) {
+        return compareNatural(a, b);
+      });
+    return sortedTopics
+        .map((String topic) => Chip(
+              label: Text(topic),
+              onDeleted: () {
+                _unsubscribeFromTopic(topic);
+              },
+            ))
+        .toList();
+  }
+
+  void _connect() async {
+    /// First create a client, the client is constructed with a broker name, client identifier
+    /// and port if needed. The client identifier (short ClientId) is an identifier of each MQTT
+    /// client connecting to a MQTT broker. As the word identifier already suggests, it should be unique per broker.
+    /// The broker uses it for identifying the client and the current state of the client. If you don’t need a state
+    /// to be hold by the broker, in MQTT 3.1.1 you can set an empty ClientId, which results in a connection without any state.
+    /// A condition is that clean session connect flag is true, otherwise the connection will be rejected.
+    /// The client identifier can be a maximum length of 23 characters. If a port is not specified the standard port
+    /// of 1883 is used.
+    /// If you want to use websockets rather than TCP see below.
+    client = mqtt.MqttClient(broker, '');
+
+    /// A websocket URL must start with ws:// or wss:// or Dart will throw an exception, consult your websocket MQTT broker
+    /// for details.
+    /// To use websockets add the following lines -:
+    /// client.useWebSocket = true;
+    /// client.port = 80;  ( or whatever your WS port is)
+    /// Note do not set the secure flag if you are using wss, the secure flags is for TCP sockets only.
+
+    /// Set logging on if needed, defaults to off
+    client.logging(true);
+
+    /// If you intend to use a keep alive value in your connect message that is not the default(60s)
+    /// you must set it here
+    client.keepAlivePeriod = 30;
+
+    /// Add the unsolicited disconnection callback
+    client.onDisconnected = _onDisconnected;
+
+    /// Create a connection message to use or use the default one. The default one sets the
+    /// client identifier, any supplied username/password, the default keepalive interval(60s)
+    /// and clean session, an example of a specific one below.
+    final mqtt.MqttConnectMessage connMess = mqtt.MqttConnectMessage()
+        .withClientIdentifier('Mqtt_MyClientUniqueId2')
+        // Must agree with the keep alive set above or not set
+        .startClean() // Non persistent session for testing
+        .keepAliveFor(30)
+        // If you set this you must set a will message
+        .withWillTopic('willtopic')
+        .withWillMessage('My Will message')
+        .withWillQos(mqtt.MqttQos.atLeastOnce);
+    print('MQTT client connecting....');
+    client.connectionMessage = connMess;
+
+    /// Connect the client, any errors here are communicated by raising of the appropriate exception. Note
+    /// in some circumstances the broker will just disconnect us, see the spec about this, we however will
+    /// never send malformed messages.
+    try {
+      await client.connect();
+    } catch (e) {
+      print(e);
+      _disconnect();
+    }
+
+    /// Check if we are connected
+    if (client.connectionState == mqtt.ConnectionState.connected) {
+      print('MQTT client connected');
+      setState(() {
+        connectionState = client.connectionState;
+      });
+    } else {
+      print('ERROR: MQTT client connection failed - '
+          'disconnecting, state is ${client.connectionState}');
+      _disconnect();
+    }
+
+    /// The client has a change notifier object(see the Observable class) which we then listen to to get
+    /// notifications of published updates to each subscribed topic.
+    subscription = client.updates.listen(_onMessage);
+  }
+
+  void _disconnect() {
+    client.disconnect();
+    _onDisconnected();
+  }
+
+  void _onDisconnected() {
+    setState(() {
+      topics.clear();
+      connectionState = client.connectionState;
+      client = null;
+      subscription.cancel();
+      subscription = null;
+    });
+    print('MQTT client disconnected');
+  }
+
+  void _onMessage(List<mqtt.MqttReceivedMessage> event) {
+    print(event.length);
+    final mqtt.MqttPublishMessage recMess =
+        event[0].payload as mqtt.MqttPublishMessage;
+    final String message =
+        mqtt.MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+    /// The above may seem a little convoluted for users only interested in the
+    /// payload, some users however may be interested in the received publish message,
+    /// lets not constrain ourselves yet until the package has been in the wild
+    /// for a while.
+    /// The payload is a byte buffer, this will be specific to the topic
+    print('MQTT message: topic is <${event[0].topic}>, '
+        'payload is <-- ${message} -->');
+    print(client.connectionState);
+    setState(() {
+      messages.add(Message(
+        topic: event[0].topic,
+        message: message,
+        qos: recMess.payload.header.qos,
+      ));
+      try {
+        messageController.animateTo(
+          0.0,
+          duration: Duration(milliseconds: 400),
+          curve: Curves.easeOut,
+        );
+      } catch (_) {
+        // ScrollController not attached to any scroll views.
+      }
+    });
+  }
+
+  void _subscribeToTopic(String topic) {
+    if (connectionState == mqtt.ConnectionState.connected) {
+      setState(() {
+        if (topics.add(topic.trim())) {
+          print('Subscribing to ${topic.trim()}');
+          client.subscribe(topic, mqtt.MqttQos.exactlyOnce);
+        }
+      });
+    }
+  }
+
+  void _unsubscribeFromTopic(String topic) {
+    if (connectionState == mqtt.ConnectionState.connected) {
+      setState(() {
+        if (topics.remove(topic.trim())) {
+          print('Unsubscribing from ${topic.trim()}');
+          client.unsubscribe(topic);
+        }
+      });
+    }
+  }
+}
