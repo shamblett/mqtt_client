2fa4c1be31c80bd16abcc9885d36a4c9b250d068
huych <>
2019-08-22 07:51:17 +0800

    fix(dart2.5): dart 2.5 error

---
+++ example/flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/huych/Documents/developTool/flutterSDK"
+export "FLUTTER_APPLICATION_PATH=/Users/huych/code/mqtt_client/example/flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/huych/Documents/developTool/flutterSDK/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
