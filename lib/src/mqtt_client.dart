5167e090bfb4c840eb1926a407c3edd94e8bf960
Steve Hamblett <steve.hamblett@linux.com>
2021-06-19 11:04:01 +0100

    Issue 289

--- lib/src/mqtt_client.dart
+++ lib/src/mqtt_client.dart
@@ -42,7 +42,7 @@ class MqttClient {
 
   /// Server name.
   /// Note that a server name that is a host name must conform to the name
-  /// syntax described in RFC952[https://datatracker.ietf.org/doc/html/rfc952]
+  /// syntax described in <a href="https://datatracker.ietf.org/doc/html/rfc952">RFC952</a>
   String server;
 
   /// Port number
