9dab80b99f7445045facdc6883e1a69d965af7dc
shamblett <steve.hamblett@linux.com>
2023-03-03 13:23:16 +0000

    Issue 245 add SocketOption class unit tests

--- lib/src/connectionhandling/mqtt_client_socket_option.dart
+++ lib/src/connectionhandling/mqtt_client_socket_option.dart
@@ -20,21 +20,51 @@ class MqttSocketOptionValue {
 }
 
 /// A class to allow socket options to be set on server TCP sockets.
+///
 /// This class supports setting of both integer and boolean raw socket options
 /// as supported by the Dart IO library [RawSocketOption](https://api.dart.dev/stable/2.19.3/dart-io/RawSocketOption-class.html) class.
+/// Please consult the documentation for the above class before using this class.
+///
 /// The socket options are set on both the initial connect and auto reconnect.
-/// Care should be used when using this feature, socket options are usually platform specific
-/// and can cause numerous failures at the network level for the unwary.
+///
+/// This class performs no sanity checking of the values provided, what values are set are
+/// passed on to the socket untouched, as such, care should be used when using this feature,
+/// socket options are usually platform specific and can cause numerous failures at the network
+/// level for the unwary.
 class MqttSocketOption {
-  MqttSocketOption(MqttSocketOptionValue option) {
-    options.add(option);
+
+  MqttSocketOption();
+
+  /// Option value
+  MqttSocketOption.fromOptionValue(MqttSocketOptionValue option) {
+    _options.add(option);
   }
 
   /// Boolean socket option
-  void fromBool(int level, int option, bool value) {}
+  void fromBool(int level, int option, bool value) {
+    final socketOptionValue = MqttSocketOptionValue();
+    socketOptionValue.type = MqttSocketOptionType.boolean;
+    socketOptionValue.level = level;
+    socketOptionValue.option = option;
+    socketOptionValue.boolValue = value;
+    _options.add(socketOptionValue);
+  }
 
   /// Integer socket option
-  void fromInt(int level, int option, int value) {}
+  void fromInt(int level, int option, int value) {
+    final socketOptionValue = MqttSocketOptionValue();
+    socketOptionValue.type = MqttSocketOptionType.integer;
+    socketOptionValue.level = level;
+    socketOptionValue.option = option;
+    socketOptionValue.intValue = value;
+    _options.add(socketOptionValue);
+  }
 
-  List<MqttSocketOptionValue> options = <MqttSocketOptionValue>[];
+  final List<MqttSocketOptionValue> _options = <MqttSocketOptionValue>[];
+
+  /// The options
+  List<MqttSocketOptionValue> get options => _options;
+
+  /// Any options set, true if any set
+  bool get hasOptions => _options.isNotEmpty;
 }
