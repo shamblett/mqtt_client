3ff74184d776104fd0dcbc197426832506c6f1fd
Steve Hamblett <steve.hamblett@linux.com>
2023-03-21 14:49:52 +0000

    Issue 454 - tests updated - all tests running (#455)

--- test/mqtt_client_subscription_manager_test.dart
+++ test/mqtt_client_subscription_manager_test.dart
@@ -11,7 +11,7 @@ import 'dart:async';
 import 'package:mqtt_client/mqtt_client.dart';
 import 'package:mqtt_client/mqtt_server_client.dart';
 import 'package:test/test.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:typed_data/typed_data.dart' as typed;
 import 'package:event_bus/event_bus.dart' as events;
 import 'support/mqtt_client_test_connection_handler.dart';
