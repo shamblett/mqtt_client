d9c0bbc559368ea288a3632c131865b0ffe4e189
Tyler Dunn <ireallylikeshrek@gmail.com>
2024-04-03 15:02:33 -0400

    Support WebAssembly compilation

--- lib/mqtt_browser_client.dart
+++ lib/mqtt_browser_client.dart
@@ -8,14 +8,17 @@
 library mqtt_browser_client;
 
 import 'dart:async';
-import 'package:universal_html/html.dart';
+import 'dart:js_interop';
 import 'dart:typed_data';
+
 import 'package:event_bus/event_bus.dart' as events;
 import 'package:typed_data/typed_data.dart' as typed;
+import 'package:web/web.dart';
+
 import 'mqtt_client.dart';
 
-part 'src/mqtt_browser_client.dart';
-part 'src/connectionhandling/browser/mqtt_client_mqtt_browser_connection_handler.dart';
-part 'src/connectionhandling/browser/mqtt_client_synchronous_mqtt_browser_connection_handler.dart';
-part 'src/connectionhandling/browser/mqtt_client_mqtt_browser_ws_connection.dart';
 part 'src/connectionhandling/browser/mqtt_client_mqtt_browser_connection.dart';
+part 'src/connectionhandling/browser/mqtt_client_mqtt_browser_connection_handler.dart';
+part 'src/connectionhandling/browser/mqtt_client_mqtt_browser_ws_connection.dart';
+part 'src/connectionhandling/browser/mqtt_client_synchronous_mqtt_browser_connection_handler.dart';
+part 'src/mqtt_browser_client.dart';
