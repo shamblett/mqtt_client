710b23b4090877c5ac054762b134a06df062eb5c
Steve Hamblett <steve.hamblett@linux.com>
2024-05-27 09:39:53 +0100

    Issue 535

---
+++ test/issues/issue535.dart
@@ -0,0 +1,21 @@
+/*
+ * Package : mqtt_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 31/05/2017
+ * Copyright :  S.Hamblett
+ */
+
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:test/test.dart';
+
+Future<int> main() {
+  test('Trigger available bytes is less', () {
+
+  };
+
+  return 0;
+}
