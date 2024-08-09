ce3394fd4f232ac9cf20811222eb414fdc692f39
Tyler Dunn <ireallylikeshrek@gmail.com>
2024-08-09 12:12:44 -0400

    Simplify conversion to JS type

--- lib/src/connectionhandling/browser/mqtt_client_mqtt_browser_ws_connection.dart
+++ lib/src/connectionhandling/browser/mqtt_client_mqtt_browser_ws_connection.dart
@@ -49,8 +49,8 @@ class MqttBrowserWsConnection extends MqttBrowserConnection<WebSocket> {
     MqttLogger.log('MqttBrowserWsConnection::connect -  WS URL is $uriString');
     try {
       // Connect and save the socket.
-      final client = WebSocket(
-          uriString, (protocols.toList()).map((e) => e.toJS).toList().toJS);
+      final client =
+          WebSocket(uriString, protocols.map((e) => e.toJS).toList().toJS);
       this.client = client;
       client.binaryType = 'arraybuffer';
       messageStream = MqttByteBuffer(typed.Uint8Buffer());
@@ -122,8 +122,8 @@ class MqttBrowserWsConnection extends MqttBrowserConnection<WebSocket> {
         'MqttBrowserWsConnection::connectAuto -  WS URL is $uriString');
     try {
       // Connect and save the socket.
-      final client = WebSocket(
-          uriString, (protocols.toList()).map((e) => e.toJS).toList().toJS);
+      final client =
+          WebSocket(uriString, protocols.map((e) => e.toJS).toList().toJS);
       this.client = client;
       client.binaryType = 'arraybuffer';
       messageStream = MqttByteBuffer(typed.Uint8Buffer());
