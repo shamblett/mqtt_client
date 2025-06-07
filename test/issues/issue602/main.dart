dfbb3e2ba1388f27552dc80bb101ed987c7da008
Steve Hamblett <steve.hamblett@linux.com>
2025-06-07 09:17:34 +0100

    Issue 602 - Test script update

--- test/issues/issue602/main.dart
+++ test/issues/issue602/main.dart
@@ -6,7 +6,7 @@ import 'package:mqtt_client/mqtt_server_client.dart';
 void main() async {
   final client = MqttServerClient('localhost', 'dart_client1111');
   client.port = 1883;
-  client.logging(on: true, logPayloads: false);
+  client.logging(on: false, logPayloads: false);
   final connMess = MqttConnectMessage();
 
   client.connectionMessage = connMess;
