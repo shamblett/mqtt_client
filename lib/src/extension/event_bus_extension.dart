7cb9f6bdd1f276c15bd81bc0825392488466ec20
Ceren Yasa <ceren.yasa@commencis.com>
2026-01-05 15:03:44 +0300

    Fix format as dart format

--- lib/src/extension/event_bus_extension.dart
+++ lib/src/extension/event_bus_extension.dart
@@ -4,9 +4,7 @@ import 'package:mqtt_client/mqtt_client.dart';
 extension EventBusExtension on EventBus? {
   void safeFire(dynamic event) {
     if (this == null || this!.streamController.isClosed) {
-      MqttLogger.log(
-        'Event not fired, event bus closed',
-      );
+      MqttLogger.log('Event not fired, event bus closed');
       return;
     }
     this!.fire(event);
--- lib/src/extension/stream_controller_extension.dart
+++ lib/src/extension/stream_controller_extension.dart
@@ -5,9 +5,7 @@ import 'package:mqtt_client/mqtt_client.dart';
 extension StreamControllerExtension on StreamController? {
   void safeAdd(dynamic event) {
     if (this == null || this!.isClosed) {
-      MqttLogger.log(
-        'Event not added, stream controller is closed',
-      );
+      MqttLogger.log('Event not added, stream controller is closed');
       return;
     }
     this!.add(event);
