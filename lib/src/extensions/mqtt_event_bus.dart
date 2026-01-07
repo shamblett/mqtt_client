817c7505d4f6bf80e0db6c84daabeee6bb8fed54
Steve Hamblett <steve.hamblett@linux.com>
2026-01-07 09:47:22 +0000

    Event bus updates

---
+++ lib/src/extensions/mqtt_event_bus.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: prefer-match-file-name
+
+/*
+ * Package : mqtt5_client
+ * Author : S. Hamblett <steve.hamblett@linux.com>
+ * Date   : 06/11/2025
+ * Copyright :  S.Hamblett
+ */
+
+part of '../../mqtt_client.dart';
+
+///
+/// Event bus extension that provides a guarded fire method.
+///
+extension type MqttEventBus._(events.EventBus eventbus) {
+  MqttEventBus.fromEventBus(this.eventbus);
+
+  void destroy() => eventbus.destroy();
+
+  Stream<T> on<T>() => eventbus.on<T>();
+
+  void fire(dynamic event) {
+    if (!eventbus.streamController.isClosed) {
+      eventbus.fire(event);
+    }
+  }
+}
